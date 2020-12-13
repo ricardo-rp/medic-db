c0134c4e7da1fd2f4e5d79700a09a7c0ab15bcf0
Moro <ricardo.romero@gmail.com>
2020-12-13 10:51:20 -0300

    corrected logo color

--- src/layouts/MainLayout/TopBar.js
+++ src/layouts/MainLayout/TopBar.js
@@ -27,7 +27,7 @@ const TopBar = ({ className, ...rest }) => {
     >
       <Toolbar className={classes.toolbar}>
         <RouterLink to="/">
-          <LocalHospital />
+          <LocalHospital style={{color: '#fce4ec'}}/>
         </RouterLink>
       </Toolbar>
     </AppBar>
