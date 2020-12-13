d59a6e2503e333b517488d1ccdeae3c8efd3499e
Moro <ricardo.romero@gmail.com>
2020-12-13 12:12:10 -0300

    changed theme to pt-BR

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,27 +1,31 @@
 import { createMuiTheme, colors } from '@material-ui/core';
 import shadows from './shadows';
 import typography from './typography';
+import { ptBR } from '@material-ui/core/locale';
 
-const theme = createMuiTheme({
-  palette: {
-    background: {
-      dark: colors.grey[200],
-      default: colors.common.white,
-      paper: colors.common.white
+const theme = createMuiTheme(
+  {
+    palette: {
+      background: {
+        dark: colors.grey[200],
+        default: colors.common.white,
+        paper: colors.common.white
+      },
+      primary: {
+        main: colors.pink[300]
+      },
+      secondary: {
+        main: colors.pink[500]
+      },
+      text: {
+        primary: colors.blueGrey[900],
+        secondary: colors.blueGrey[600]
+      }
     },
-    primary: {
-      main: colors.pink[300]
-    },
-    secondary: {
-      main: colors.pink[500]
-    },
-    text: {
-      primary: colors.blueGrey[900],
-      secondary: colors.blueGrey[600]
-    }
+    shadows,
+    typography
   },
-  shadows,
-  typography
-});
+  ptBR
+);
 
 export default theme;
