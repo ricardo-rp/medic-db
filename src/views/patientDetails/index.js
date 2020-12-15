3dbc9a342efd928d055c6623d29429cba8f985aa
Moro <ricardo.romero@gmail.com>
2020-12-15 07:32:17 -0300

    Created card

--- src/views/patientDetails/index.js
+++ src/views/patientDetails/index.js
@@ -1,12 +1,10 @@
 import React, { useState } from 'react';
-import {
-  Box,
-  Container,
-  makeStyles
-} from '@material-ui/core';
+import PerfectScrollbar from 'react-perfect-scrollbar';
+import { Box, Card, Container, makeStyles } from '@material-ui/core';
 import Page from 'src/components/Page';
+import { useParams } from 'react-router-dom';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   root: {
     backgroundColor: theme.palette.background.dark,
     minHeight: '100%',
@@ -18,13 +16,17 @@ const useStyles = makeStyles((theme) => ({
 const PatientView = () => {
   const classes = useStyles();
 
+  let { id } = useParams();
+
   return (
-    <Page
-      className={classes.root}
-      title="Customers"
-    >
+    <Page className={classes.root} title="Customers">
       <Container maxWidth={false}>
         <Box mt={3}>
+          <Card>
+            <PerfectScrollbar>
+              <Box minWidth={1050}>{`${id}`}</Box>
+            </PerfectScrollbar>
+          </Card>
         </Box>
       </Container>
     </Page>
