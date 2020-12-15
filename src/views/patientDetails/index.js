642ddde84d3a70fc0a2619b1f9268dffebfe1732
Moro <ricardo.romero@gmail.com>
2020-12-15 07:50:47 -0300

    copied form template from registerForm

--- src/views/patientDetails/index.js
+++ src/views/patientDetails/index.js
@@ -1,8 +1,23 @@
 import React, { useState } from 'react';
+import { useNavigate, useParams, Link as RouterLink } from 'react-router-dom';
+
 import PerfectScrollbar from 'react-perfect-scrollbar';
-import { Box, Card, Container, makeStyles } from '@material-ui/core';
+import {
+  Box,
+  Card,
+  Checkbox,
+  Container,
+  Link,
+  makeStyles,
+  TextField,
+  Typography,
+  FormHelperText,
+  Button
+} from '@material-ui/core';
 import Page from 'src/components/Page';
-import { useParams } from 'react-router-dom';
+
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -15,16 +30,165 @@ const useStyles = makeStyles(theme => ({
 
 const PatientView = () => {
   const classes = useStyles();
+  const navigate = useNavigate();
 
   let { id } = useParams();
 
   return (
-    <Page className={classes.root} title="Customers">
+    <Page className={classes.root} title="Paciente">
       <Container maxWidth={false}>
         <Box mt={3}>
           <Card>
             <PerfectScrollbar>
-              <Box minWidth={1050}>{`${id}`}</Box>
+              <Box minWidth={1050}>
+                {`${id}`}
+                <Formik
+                  initialValues={{
+                    email: '',
+                    fullName: '',
+                    motherName: '',
+                    password: '',
+                    policy: false
+                  }}
+                  validationSchema={Yup.object().shape({
+                    fullName: Yup.string()
+                      .max(255)
+                      .required('Nome completo é requerido'),
+                    motherName: Yup.string()
+                      .max(255)
+                      .required('Nome da mãe é requerido'),
+                    email: Yup.string()
+                      .email('Must be a valid email')
+                      .max(255)
+                      .required('Email is required'),
+                    password: Yup.string()
+                      .max(255)
+                      .required('password is required'),
+                    policy: Yup.boolean().oneOf(
+                      [true],
+                      'This field must be checked'
+                    )
+                  })}
+                  onSubmit={() => {
+                    navigate('/app/dashboard', { replace: true });
+                  }}
+                >
+                  {({
+                    errors,
+                    handleBlur,
+                    handleChange,
+                    handleSubmit,
+                    isSubmitting,
+                    touched,
+                    values
+                  }) => (
+                    <form onSubmit={handleSubmit}>
+                      <Box mb={3}>
+                        <Typography color="textPrimary" variant="h2">
+                          Create new account
+                        </Typography>
+                        <Typography
+                          color="textSecondary"
+                          gutterBottom
+                          variant="body2"
+                        >
+                          Use your email to create new account
+                        </Typography>
+                      </Box>
+                      <TextField
+                        error={Boolean(touched.fullName && errors.fullName)}
+                        fullWidth
+                        helperText={touched.fullName && errors.fullName}
+                        label="First name"
+                        margin="normal"
+                        name="firstName"
+                        onBlur={handleBlur}
+                        onChange={handleChange}
+                        value={values.fullName}
+                        variant="outlined"
+                      />
+                      <TextField
+                        error={Boolean(touched.motherName && errors.motherName)}
+                        fullWidth
+                        helperText={touched.motherName && errors.motherName}
+                        label="Last name"
+                        margin="normal"
+                        name="lastName"
+                        onBlur={handleBlur}
+                        onChange={handleChange}
+                        value={values.motherName}
+                        variant="outlined"
+                      />
+                      <TextField
+                        error={Boolean(touched.email && errors.email)}
+                        fullWidth
+                        helperText={touched.email && errors.email}
+                        label="Email Address"
+                        margin="normal"
+                        name="email"
+                        onBlur={handleBlur}
+                        onChange={handleChange}
+                        type="email"
+                        value={values.email}
+                        variant="outlined"
+                      />
+                      <TextField
+                        error={Boolean(touched.password && errors.password)}
+                        fullWidth
+                        helperText={touched.password && errors.password}
+                        label="Password"
+                        margin="normal"
+                        name="password"
+                        onBlur={handleBlur}
+                        onChange={handleChange}
+                        type="password"
+                        value={values.password}
+                        variant="outlined"
+                      />
+                      <Box alignItems="center" display="flex" ml={-1}>
+                        <Checkbox
+                          checked={values.policy}
+                          name="policy"
+                          onChange={handleChange}
+                        />
+                        <Typography color="textSecondary" variant="body1">
+                          I have read the{' '}
+                          <Link
+                            color="primary"
+                            component={RouterLink}
+                            to="#"
+                            underline="always"
+                            variant="h6"
+                          >
+                            Terms and Conditions
+                          </Link>
+                        </Typography>
+                      </Box>
+                      {Boolean(touched.policy && errors.policy) && (
+                        <FormHelperText error>{errors.policy}</FormHelperText>
+                      )}
+                      <Box my={2}>
+                        <Button
+                          color="primary"
+                          disabled={isSubmitting}
+                          fullWidth
+                          size="large"
+                          type="submit"
+                          variant="contained"
+                        >
+                          Sign up now
+                        </Button>
+                      </Box>
+                      <Typography color="textSecondary" variant="body1">
+                        Have an account?{' '}
+                        <Link component={RouterLink} to="/login" variant="h6">
+                          Sign in
+                        </Link>
+                      </Typography>
+                    </form>
+                  )}
+                </Formik>
+              </Box>
             </PerfectScrollbar>
           </Card>
         </Box>
