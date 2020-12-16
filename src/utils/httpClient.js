a6422f5efe744fea715a3a251173b9bc678c0eed
Moro <ricardo.romero@gmail.com>
2020-12-15 23:44:39 -0300

    changed mock api to mockaroo

--- src/utils/httpClient.js
+++ src/utils/httpClient.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
-const URL = 'https://mockend.com/org/medic-db/';
+const URL = 'https://my.api.mockaroo.com/';
 
 export default axios.create({
   baseURL: URL,
   timeout: 10000,
-  headers: { 'Access-Control-Allow-Origin': '*' }
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'X-API-Key': '38effba0'
+  }
 });
