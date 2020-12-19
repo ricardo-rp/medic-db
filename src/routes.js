70dbceb0d183b0b828c515a96577a634437d76fd
Moro <ricardo.romero@gmail.com>
2020-12-19 08:17:33 -0300

    removed unused views

--- src/routes.js
+++ src/routes.js
@@ -6,10 +6,7 @@ import MainLayout from 'src/layouts/MainLayout';
 
 import PatientListView from 'src/views/patientList';
 import DashboardView from 'src/views/reports/DashboardView';
-import LoginView from 'src/views/auth/LoginView';
 import NotFoundView from 'src/views/errors/NotFoundView';
-import RegisterView from 'src/views/auth/RegisterView';
-import SettingsView from 'src/views/settings/SettingsView';
 import PatientView from 'src/views/patientDetails';
 
 const routes = [
@@ -19,7 +16,6 @@ const routes = [
     children: [
       { path: 'dashboard', element: <DashboardView /> },
       { path: 'patient', element: <PatientListView /> },
-      { path: 'settings', element: <SettingsView /> },
       { path: 'patient/:id', element: <PatientView /> },
       { path: '*', element: <Navigate to="/404" /> }
     ]
@@ -28,8 +24,6 @@ const routes = [
     path: '/',
     element: <MainLayout />,
     children: [
-      { path: 'login', element: <LoginView /> },
-      { path: 'register', element: <RegisterView /> },
       { path: '404', element: <NotFoundView /> },
       { path: '/', element: <Navigate to="/app/dashboard" /> },
       { path: '*', element: <Navigate to="/404" /> }
